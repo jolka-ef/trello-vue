1de8a46c78fc3922e501fba200958e98f2208511
jolka ef <firjola@gmail.com>
2024-03-11 18:04:16 +0100

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useBoardStore } from '~/stores/boardStore';
+
+const boardStore = useBoardStore();
+</script>
+<template>
+    <main>
+        <h1>Trello Clone</h1>
+        <pre>{{ boardStore.board }}</pre>
+    </main>
+</template>
