86b1263baea2e313d9b886cc7ab1585c893ebeb5
jolka ef <firjola@gmail.com>
2024-03-12 13:27:59 +0100

    config : fix typos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css : ['~/assets/css/vallo.css',]
+  css : ['~/assets/css/vello.css'],
   devtools: { enabled: true },
   modules : ['@nuxt/ui','@pinia/nuxt'],
   colorMode: {
