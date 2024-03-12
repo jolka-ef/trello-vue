5f3f576eb585e17c1e7e948eee8f3ad01d80c56f
jolka ef <firjola@gmail.com>
2024-03-12 13:22:23 +0100

    config : add vello.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css : ['~/assets/css/vallo.css',]
   devtools: { enabled: true },
   modules : ['@nuxt/ui','@pinia/nuxt'],
   colorMode: {
