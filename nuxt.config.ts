d4c8ee2dd98051dc0206c4d9ad0c83bce6c22ffe
jolka ef <firjola@gmail.com>
2024-03-12 20:50:46 +0100

    config : nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css : ['~/assets/css/vello.css'],
+  ssr: false,
+  css: ["~/assets/css/vello.css"],
   devtools: { enabled: true },
-  modules : ['@nuxt/ui','@pinia/nuxt'],
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@vueuse/nuxt"],
   colorMode: {
-    preference: 'light'
-  }
-})
+    preference: "light",
+  },
+});
